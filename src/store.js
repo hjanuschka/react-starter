77d962181e9685f3815b06aec0e4a31666d3a0eb
Helmut Januschka <h.januschka@krone.at>
2017-03-07 11:14:04 +0100

    devtools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from "redux"
+import { applyMiddleware, createStore, compose } from "redux"
 
 import logger from "redux-logger"
 import thunk from "redux-thunk"
@@ -6,6 +6,8 @@ import promise from "redux-promise-middleware"
 
 import reducer from "./reducers"
 
-const middleware = applyMiddleware(promise(), thunk, logger())
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middleware = composeEnhancers(applyMiddleware(promise(), thunk, logger()))
 
 export default createStore(reducer, middleware)
