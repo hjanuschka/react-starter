d7aa526094db47db8387bf8d3e7290d6e1964a36
Helmut Januschka <h.januschka@krone.at>
2017-03-07 08:43:33 +0100

    u

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Router, Route } from 'react-router';
 
-import Login from './components/Login';
-import About from './components/About';
-import NotFound from './components/NotFound';
+import Login from './containers/Login';
+import About from './containers/About';
+import NotFound from './containers/NotFound';
 
 const Routes = (props) => (
   <Router {...props}>
